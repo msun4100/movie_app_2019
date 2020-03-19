a1e0a41f75711c15d5c84eb0ce634b11d19aa776
msun4100 <msun4100@gmail.com>
2020-03-19 19:36:10 +0900

    Updates

---
+++ precache-manifest.2a93b0146fd5a978feaca36d71057df5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c56143347477c34321974dd5dc392c2",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "15c7a04d1ed283195181",
+    "url": "/movie_app_2019/static/css/main.93517c7d.chunk.css"
+  },
+  {
+    "revision": "f58267d5d93c4af09dec",
+    "url": "/movie_app_2019/static/js/2.fe7ecb05.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/movie_app_2019/static/js/2.fe7ecb05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15c7a04d1ed283195181",
+    "url": "/movie_app_2019/static/js/main.09550e49.chunk.js"
+  },
+  {
+    "revision": "bc42afcb986eef44e7a7",
+    "url": "/movie_app_2019/static/js/runtime-main.30c24a73.js"
+  }
+]);
